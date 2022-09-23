6f76936c30e160c3d6660cb339cb61eafc2cf452
SudKul <sudhanshu.kulshrestha@gmail.com>
2022-09-23 17:56:04 +0530

    Update the folder structure

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,21 +1,21 @@
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
-resource "azurerm_app_service" "test" {
+resource "azurerm_linux_web_app" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  app_service_plan_id = azurerm_app_service_plan.test.id
+  service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
+  }
+  site_config {
+    always_on = false
   }
 }
