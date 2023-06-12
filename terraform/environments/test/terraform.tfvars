4e49189fc6d0643a87449370d949ce4def43a5ca
Wojciech Siwecki <wsiwecki@gmail.com>
2023-06-12 16:26:50 +0200

    correct VM definition

--- terraform/environments/test/terraform.tfvars
+++ terraform/environments/test/terraform.tfvars
@@ -1,15 +1,17 @@
 # Azure subscription vars
-subscription_id = "0ee6d06f-69ab-4b3b-9f35-003e1b6eb227"
-client_id = "b1e6e530-f258-47c6-a5d0-3173516058c5"
-client_secret = "NMH8Q~5D3SPPeWNA.~3Te1LYz1dYRpQay87.Wauc"
+subscription_id = "a3a536c2-3690-4c5f-8032-998a8dc3234e"
+client_id = "e5a26397-e26b-49a6-af6a-8a9501bbbb35"
+client_secret = "ZVq8Q~WVCTKU~u7dLwXr1-hSlT.OZ-~6o7P9zbid"
 tenant_id = "f958e84a-92b8-439f-a62d-4f45996b6d07"
 
 # Resource Group/Location
-location = "East US"
+location = "West Europe"
 resource_group_name = "Azuredevops"
+resource_group = "Azuredevops"
 application_type = "myApplication"
 
 # Network
-virtual_network_name = ""
+virtual_network_name = "myVNet"
 address_space = ["10.5.0.0/16"]
+address_prefixes_test = ["10.5.1.0/24"]
 address_prefix_test = "10.5.1.0/24"
--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,30 +1,17 @@
 
-variable "rg_name" {
-   default     = "Azuredevops"
-   description = "Name of the resource group."
+variable "location" {
+  default = "westeurope"
+}
+variable "application_type" {
+  default =  "myApplication"
+}
+variable "resource_type" {
+  default =  "VM"
+}
+variable "resource_group" {
+  default =  "Azuredevops"
 }
 
-variable "rg_location" {
-  default     = "eastus"
-  description = "Location of the resource group."
-}
-
-variable "admin_username" {
-  default     = "adminuser"
-  description = "Location of the resource group."
-}
-
-variable "vm_name" {
-  default     = "myVM"
-  description = "Location of the resource group."
-}
-
-variable "nic_name" {
-  default     = "myNetworkInterface"
-  description = "Location of the resource group."
-}
-
-variable "rg_prefix" {	
-  default     = "myRg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+variable "resource_group" {
+  default =  "Azuredevops"
 }
