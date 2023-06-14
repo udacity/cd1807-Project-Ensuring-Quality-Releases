6c924033b8b49aadb3e3b201db96bb32abf5f2f4
Wojciech Siwecki <wsiwecki@gmail.com>
2023-06-14 14:58:11 +0200

    update ssh vars

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -44,6 +44,11 @@ resource "azurerm_linux_virtual_machine" "test" {
 #    username   = "adminuser"
 #    public_key = "file("~/.ssh/id_rsa.pub")"
 #  }
+#  admin_ssh_key {
+#    username   = "adminuser"
+#    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCe+85RdeEz+9UVLdmToflPtHREOLFpODV/HR1aQoyP+veU4dEYF/gdvHkHOCBNl/x9wNeitr9tzrc0AGjy2hEFGjRdOrT7XG1nZ6a6Mhjd9yTT3ghDk8p8ipYQqbmqyTQphNT9ym4qSOJ1zakCjM6a4M7HB7AyhS8ddI1xGxPH5q2XSh5HC95/1hxrgXFakjNT2Zw//D9LT2gQdIZuuBUPE/8+SjEPa97Qe8m6W9WCkulEFzLzK0TsM6cQ8y0eCrmh30lWfT7XHm2w0kLvj4POL8+2BCCb+dcbBxV/oH4Z9xemBk84psFbixHIVUNIbJuYQORPlWf9wzCD7mjti0C1HFGBjCQyTqDaYjXW91ifIotJiIGILeZ1ZA7XiC9dbiJF8pwPsi3OjT3ehcxrue19/f7mTJ07CJIqTIKF6DL0CLmfZj+ae44jmysihCdJvbia+pRgvwlH5fm9551LL3F3J4O1xRIBIKLSwbiHZqUxEzu8UtwIOyafez0HdWWFRkc= wojsiw@DESKTOP-AVL1658"
+#  }
+
   os_disk {
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
