e92511ff6a542682a5b9a862667ecf35bbe17f72
Wojciech Siwecki <wsiwecki@gmail.com>
2023-07-20 22:56:04 +0200

    correct syntax

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -4,4 +4,5 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 # Start the browser and navigate to webpage
 driver = webdriver.Chrome()
-driver.get(https://wswebapplication-appservice.azurewebsites.net/automatedtesting/selenium/login.py')
+driver.get('https://wswebapplication-appservice.azurewebsites.net/automatedtesting/selenium/login.py')
+#driver.get(https://wswebapplication-appservice.azurewebsites.net/automatedtesting/selenium/login.py')
