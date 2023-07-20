81d76b75d84cde9300cb61307dc5dcc651220fb4
Wojciech Siwecki <wsiwecki@gmail.com>
2023-07-20 22:39:48 +0200

    fill selenium

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -1,3 +1,7 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+# Start the browser and navigate to webpage
+driver = webdriver.Chrome()
+driver.get('http://wswebapplication-appservice.azurewebsites.net')
